9bb302993d8c02275900fd35c8e4875b709f6ab3
emmepi97 <matteopoggesi23@gmail.com>
2026-07-06 13:56:09 +0200

    Delete app directory

--- app/layout.js
+++
@@ -1,7 +0,0 @@
-import './globals.css';
-
-export const metadata = { title: 'CV Online', description: 'CV online aggiornabile con Supabase e Vercel' };
-
-export default function RootLayout({ children }) {
-  return <html lang="it"><body>{children}</body></html>;
-}
