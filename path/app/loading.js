d9423e57b91413650605284c8b11e6ca882fe7ed
emmepi97 <matteopoggesi23@gmail.com>
2026-07-24 08:54:53 +0200

    Delete path directory

--- path/app/loading.js
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="routeLoading" role="status" aria-label="Caricamento">
-      <div className="routeLoadingSpinner" />
-    </div>
-  );
-}
